528c5dee95735e6446946cb8481703009fe69d70
Unknown <ormaipeter.fx@gmail.com>
2018-02-15 15:54:10 +0100

    Create service layer

---
+++ src/main/java/com/museme/service/AccountService.java
@@ -0,0 +1,10 @@
+package com.museme.service;
+
+import com.museme.model.account.Account;
+
+public interface AccountService {
+
+    void saveUser(Account account);
+
+    Account findUserByEmail(String email);
+}
