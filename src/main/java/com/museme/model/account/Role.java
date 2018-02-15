9a91cb04b17b708161fe656681350f36bc3fcb12
Unknown <ormaipeter.fx@gmail.com>
2018-02-15 11:05:37 +0100

    Create Role class

---
+++ src/main/java/com/museme/model/account/Role.java
@@ -0,0 +1,41 @@
+package com.museme.model.account;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Long id;
+
+    @Column(name = "role")
+    private String role;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<Account> accounts = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Set<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(Set<Account> accounts) {
+        this.accounts = accounts;
+    }
+}
