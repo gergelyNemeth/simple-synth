9ad2ccce3f17440773eafd825de5afc6d58d36f2
Gergely Németh <ngerle@gmail.com>
2018-02-16 03:00:33 +0100

    Add Jsr310JpaConverters with an annotation to the application for working date column in the database

--- src/main/java/com/museme/model/account/Account.java
+++ src/main/java/com/museme/model/account/Account.java
@@ -3,6 +3,7 @@ package com.museme.model.account;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
